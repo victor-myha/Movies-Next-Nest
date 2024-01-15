a0da739066b87e9d30d26f5be88ba6a2d1540a4b
victor-myha-serec <viktor@seractech.co.uk>
2024-01-15 10:15:57 +0200

    [add] configurations for aws; enabled cors; add healthCheck to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, MoviesModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
